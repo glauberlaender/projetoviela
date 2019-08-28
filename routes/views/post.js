02ccfce3808a106699caad4db01eb3fbb1fb39b7
Henrique Aveiro <henrique.aveiro@globalhitss.com.br>
2019-08-28 19:19:23 -0300

    Two columns fix

--- routes/views/post.js
+++ routes/views/post.js
@@ -13,20 +13,21 @@ exports = module.exports = function (req, res) {
 	locals.data = {
 		posts: [],
 	};
-
+	
 	// Load the current post
 	view.on('init', function (next) {
-
+		
 		var q = keystone.list('Post').model.findOne({
 			state: 'published',
 			slug: locals.filters.post,
 		}).populate('author categories');
-
+		
+		
 		q.exec(function (err, result) {
 			locals.data.post = result;
 			next(err);
 		});
-
+		
 	});
 
 	// Load other posts
